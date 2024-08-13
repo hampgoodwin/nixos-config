c128422d5cd5e1a60b21bcd25ff742c7d6e33e6d
hampgoodwin <hampgoodwin@gmail.com>
2024-08-12 20:10:17 -0600

    update to flake

---
+++ configuration.nix
@@ -0,0 +1,137 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{
+  inputs,
+  config,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    # third-party non-core
+    # local import
+    ./audio.nix
+    ./sh.nix
+    ./hamp.nix
+    ./hypr.nix
+    ./steam.nix
+    ./sunshine.nix
+    ./developer.nix
+  ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Shell, uses zsh
+  # from import
+  sh.enable = true;
+  # hamp use from import
+  hamp.user.enable = true;
+  # Audio from import
+  audio = {
+    enable = true;
+    bluetooth.enable = true;
+    packages.enable = true;
+    lowLatency.enable = true;
+  };
+  developer.enable = true;
+
+  networking.hostName = "hamp"; # Define your hostname.
+
+  # Enable wireless networking/lan via network manager
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "America/Denver";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver = {
+    xkb.layout = "us";
+    xkb.variant = "";
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  nixpkgs.overlays = [ inputs.fh.overlays.default ];
+  # Enable flakes and accompanying nix cli tools
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+  # List packages installed in system profile. To search, run:
+  environment.systemPackages = with pkgs; [
+    fh
+    # keyboard
+    # keymapp # doesn't work
+    # zsa-udev-rules # doesn't work; add custom udev rules?
+    # utilities
+    util-linux
+    killall
+    firefox
+    enpass
+    xfce.thunar
+    orca-slicer
+    unzip
+    wl-clipboard
+    filezilla
+    grim
+    swappy
+    # communication
+    slack
+    vesktop
+    xwaylandvideobridge
+    # bar
+    waybar
+    waybar-mpris
+    # widgets
+    # notifications
+    dunst
+    libnotify
+    # launcher
+    rofi-wayland
+  ];
+
+  fonts.packages = with pkgs; [ (nerdfonts.override { fonts = [ "JetBrainsMono" ]; }) ];
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+
+}
---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.fh.url = "github:DeterminateSystems/fh";
+  # instead of using fh's nixpkgs (dependencies) we can inform it to follow
+  # nixpkgs to reduce closure size; this means that if a package at a version
+  # exists in both fh and nixpkgs, fh inputs are instructed to follow nixpkgs
+  # builds vs created its own
+  inputs.fh.inputs.nixpkgs.follows = "nixpkgs";
+  # if I want to get into hm we can enable this to get started
+  # inputs.home-manager.url = github:nix-community/home-manager;
+  # inputs.home-manager.nixpkgs.follows = "nixpkgs"; 
+
+  outputs =
+    inputs: with inputs; {
+      nixosConfigurations.hamp = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {
+          inherit inputs;
+        };
+        modules = [ ./configuration.nix ];
+      };
+    };
+}
