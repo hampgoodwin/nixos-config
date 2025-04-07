333cf4433aafcc09b22642ca007b46355fbbc17f
hampgoodwin <hampgoodwin@gmail.com>
2025-04-07 12:33:47 -0600

    upd flake deps, rebuild

--- configurations/ghost/configuration.nix
+++ configurations/ghost/configuration.nix
@@ -1,6 +1,7 @@
 {
   description = "ghost configuration";
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
